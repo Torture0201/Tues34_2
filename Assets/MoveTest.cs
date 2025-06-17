47fff0a1458e3b11b80d9617cf16d57105f15683
Torture0201 <kaiton2721@gmail.com>
2025-06-17 16:28:17 +0900

    Sprite Test

---
+++ Assets/MoveTest.cs
@@ -0,0 +1,40 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+public class MoveTest : MonoBehaviour
+{
+    //  拡大スケール
+    [SerializeField] private float _scale = 1.0f;
+
+    //　拡縮中の場合ture
+    private bool _isAction = false;
+    //　拡大方向か縮小方向かの判別フラグ
+    //　ture: 拡大, false:縮小
+    private bool _isBig = true;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // このスクリプトがはありついているオブジェクトの座標を指定
+        transform.position = new Vector3(0.0f, -0.0f, 0);   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //  毎フレーム右に 0.01 m(10cm)移動する
+        //transform.position +- new Vector3(
+        transform.Rotate(0, 0, -1f);
+        if(_isAction is false)
+        {
+            _isAction = true;
+            if (_isBig)
+                transform.DOScale(Vector3.one * _scale, 1f)
+                    .OnComplete(() => _isAction = false);
+            else
+                transform.DOScale(Vector3.one, 1f)
+                    .OnComplete(() => _isAction = false);
+            _isBig = !_isBig;
+        }
+    }
+}
---
+++ Assets/MoveTest2.cs
@@ -0,0 +1,22 @@
+﻿using UnityEditor.ShaderGraph.Internal;
+using UnityEngine;
+
+public class MoveTest2 : MonoBehaviour
+{
+    // SerializeField はインスペクター（情報窓）に指定した変数などを
+    //　表示して外部から入れ替えできるようにする命令
+    [SerializeField] private float _rotateSpeed;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 外部で設定したスピードで回転させる
+        transform.Rotate(0f,0f,_rotateSpeed);
+    }
+}
